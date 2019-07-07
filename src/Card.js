49f00d14ec323acbe4c11a7e4796aa914620ec11
Carlos De Orta <deortacarlos@icloud.com>
2019-07-07 02:03:59 -0700

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Card.css';
+
+class Card extends React.Component {
+    render() {
+        console.log(this.props);
+
+        return ( 
+            <div className = "Card" >
+            <button type ='button'> Delete </button> 
+            <h3> {this.props.title} </h3> 
+            <p> {this.props.content} </p> 
+            </div>
+        )
+    }
+}
+
+export default Card;
